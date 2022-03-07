5055be3e7aa35d59117986883358b2d88b1fdbd7
Lucas Oliveira Sousa Dias Rosendo <lucasrosendo91@gmail.com>
2022-03-07 17:48:43 -0300

    Requisito 2

--- index.js
+++ index.js
@@ -26,6 +26,24 @@ app.get('/talker', async (req, res) => {
   }
 });
 
+app.get('/talker/:id', (_req, res) => {
+  try {
+    const { id } = _req.params;
+    fs.readFile(FILENAME).then((data) => {
+      const talkers = JSON.parse(data);
+      const talker = talkers.find((t) => t.id === parseInt(id, 10));
+
+      if (!talker) {
+        return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+      }
+
+      return res.status(200).send(talker);
+    });
+  } catch (e) {
+    return res.status(500).end();
+  }
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
